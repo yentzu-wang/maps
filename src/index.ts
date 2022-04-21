cc991158a2c82cc0763c886abaed346799217dfd
Frank Wang <frank7300228@gmail.com>
2022-04-22 00:13:48 +0800

    Finish 64

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,8 @@ const user = new User()
 const company = new Company()
 console.log(user)
 console.log(company)
+
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: { lat: 0, lng: 0 }
+})
